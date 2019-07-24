fea56bc96b1db302779511ca704dac9e3106039d
Robert Stoll <robstoll@tegonal.com>
2019-07-24 08:53:37 +0200

    better mark last commit

--- scripts/clone-current.sh
+++ scripts/clone-current.sh
@@ -4,4 +4,7 @@ set -e
 echo "git clone --depth 1 --single-branch --branch $CI_COMMIT_REF_NAME $GITBOT_REPO_URL"
 git clone --depth 1 --single-branch --branch $CI_COMMIT_REF_NAME $GITBOT_REPO_URL
 cd $CI_PROJECT_NAME
+echo " ============= last commit ================"
+commit 99f76fe78286fcdeba88789ee06e7e1e8981b589
 git --no-pager log
+echo "==========================================="
