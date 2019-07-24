3858e5d634859cc4d218bae2e82ff50bb91e5707
Robert Stoll <robstoll@tegonal.com>
2019-07-24 10:16:31 +0200

    remove command from clone-current

--- scripts/clone-current.sh
+++ scripts/clone-current.sh
@@ -5,6 +5,5 @@ echo "git clone --depth 1 --single-branch --branch $CI_COMMIT_REF_NAME $GITBOT_R
 git clone --depth 1 --single-branch --branch $CI_COMMIT_REF_NAME $GITBOT_REPO_URL
 cd $CI_PROJECT_NAME
 echo " ============= last commit ================"
-commit 99f76fe78286fcdeba88789ee06e7e1e8981b589
 git --no-pager log
 echo "==========================================="
